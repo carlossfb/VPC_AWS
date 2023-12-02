f1189b9b2934c83139d5c9ad548c11d1c8902692
carlossfb <carloseduardofreireb@gmail.com>
2023-12-02 11:12:26 -0300

    Refactor: trazendo a output subnet para Lista

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "subnet_id" {
-  value = { for key, subnet in aws_subnet.sub : key => subnet.id }
+  value = values({ for key, subnet in aws_subnet.sub : key => subnet.id })
 }
 output "security_group_id" {
   value = aws_security_group.sg_main.id
